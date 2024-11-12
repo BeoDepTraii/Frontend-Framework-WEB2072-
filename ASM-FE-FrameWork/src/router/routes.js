c72367d87414007edd914e429e013c923dcc6c18
Beo Học Ngu <100513440+BeoDepTraii@users.noreply.github.com>
2024-11-12 14:01:14 +0700

    add asm folder

---
+++ ASM-FE-FrameWork/src/router/routes.js
@@ -0,0 +1,82 @@
+import DashboardLayout from "@/layout/dashboard/DashboardLayout.vue";
+// GeneralViews
+import NotFound from "@/pages/NotFoundPage.vue";
+
+// Admin pages
+import Dashboard from "@/pages/Dashboard.vue";
+import UserProfile from "@/pages/UserProfile.vue";
+import Notifications from "@/pages/Notifications.vue";
+import Icons from "@/pages/Icons.vue";
+import Maps from "@/pages/Maps.vue";
+import Typography from "@/pages/Typography.vue";
+import ProductManage from "@/pages/ProductManage.vue";
+import OrderManage from "@/pages/OrderManage.vue";
+import UserManage from "@/pages/UserManage.vue";
+
+
+const routes = [
+  {
+    path: "/",
+    component: DashboardLayout,
+    redirect: "/dashboard",
+    children: [
+      {
+        path: "dashboard",
+        name: "dashboard",
+        component: Dashboard,
+      },
+      {
+        path: "stats",
+        name: "stats",
+        component: UserProfile,
+      },
+      {
+        path: "notifications",
+        name: "notifications",
+        component: Notifications,
+      },
+      {
+        path: "icons",
+        name: "icons",
+        component: Icons,
+      },
+      {
+        path: "maps",
+        name: "maps",
+        component: Maps,
+      },
+      {
+        path: "typography",
+        name: "typography",
+        component: Typography,
+      },
+      {
+        path: "product-manage",
+        name: "product-manage",
+        component: ProductManage,
+      },
+      {
+        path: "order-manage",
+        name: "order-manage",
+        component: OrderManage,
+      },
+      {
+        path: "user-manage",
+        name: "user-manage",
+        component: UserManage,
+      },
+    ],
+  },
+  { path: "*", component: NotFound },
+];
+
+/**
+ * Asynchronously load view (Webpack Lazy loading compatible)
+ * The specified component must be inside the Views folder
+ * @param  {string} name  the filename (basename) of the view to load.
+function view(name) {
+   var res= require('../components/Dashboard/Views/' + name + '.vue');
+   return res;
+};**/
+
+export default routes;
